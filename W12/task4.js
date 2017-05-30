1b88d1277fc75e2afa62803e1a3c707a92900df4
小磯　一貴 <1465033t@istc-a-068.epc.center.kobe-u.ac.jp>
2017-05-30 13:04:28 +0900

    Initial commit

---
+++ W12/task4.js
@@ -0,0 +1,30 @@
+function main()
+{
+    var volume = new KVS.LobsterData();
+    var screen = new KVS.THREEScreen();
+
+    screen.init( volume, {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        enableAutoResize: false
+    });
+
+    var bounds = Bounds( volume );
+    screen.scene.add( bounds );
+
+   // var isovalue = 128;
+    var point = new THREE.Vector3(60,60,17);
+    var normal = new THREE.Vector3(0,0,1);
+    var surfaces = SlicePlane( volume, point, normal );
+    screen.scene.add( surfaces );
+
+    document.addEventListener( 'mousemove', function() {
+        screen.light.position.copy( screen.camera.position );
+    });
+
+    window.addEventListener( 'resize', function() {
+        screen.resize( [ window.innerWidth, window.innerHeight ] );
+    });
+
+    screen.loop();
+}
